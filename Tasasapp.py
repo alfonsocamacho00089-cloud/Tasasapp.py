882d7430b9b9060893f292bc58a9ce5216ddb533
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-03-31 12:09:07 -0400

    Update Tasasapp.py

--- Tasasapp.py
+++ Tasasapp.py
@@ -1,8 +1,7 @@
 import requests
 import json
 import datetime
-from pyDolarVenezuela.pages import Monitor
-
+from pyDolarVenezuela import Monitor
 # Mantenemos tus headers por seguridad
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
